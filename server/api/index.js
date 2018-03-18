0f27fbb95322fbe69d0b1ad1c65eb30675ef4273
Ivan <madritistaaa@gmail.com>
2018-03-18 09:22:13 +0200

    Events module (#25)
    * Scaffold event model
    
    * Change dev script start file
    
    * Rename model file
    
    * Scaffold event module files
    
    * Configure events module exports
    
    * Import events module in api index

--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 
+import eventRouterInit from './events'
+
 export default ({ config, db }) => {
 	const router = Router();
+
+	const eventRouter = eventRouterInit(db, config);
+	router.use('/events', eventRouter);
 
 	return router;
 }
