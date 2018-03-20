a728b80566dfcd5e83663d43d9ca93a1eeb3e74c
Ivan <madritistaaa@gmail.com>
2018-03-20 18:29:33 +0200

    Users module (#28)
    * Scaffold user module
    
    * Add properties to user model

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 
 import eventRouterInit from './events'
+import userRouterInit from './users'
 
 export default (app, config, db) => {
 	const router = Router();
 
 	const eventRouter = eventRouterInit(db, config);
 	router.use('/events', eventRouter);
+
+	const userRouter = userRouterInit(db, config);
+	router.use('/users', userRouter);
 
 	app.use('/api', router);
 }
