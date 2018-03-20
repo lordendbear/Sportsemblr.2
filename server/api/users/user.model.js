76753c6f00128fe9b661033f26590b300acdf1a9
Ivan <madritistaaa@gmail.com>
2018-03-20 18:08:18 +0200

    Add properties to user model

--- server/api/users/user.model.js
+++ server/api/users/user.model.js
@@ -1,8 +1,42 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({}, {
-    timestamps: true
-});
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        match: /^\S+@\S+\.\S+$/,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        maxlength: 128,
+    },
+    name: {
+        type: String,
+        maxlength: 128,
+        index: true,
+        trim: true,
+    },
+    services: {
+        // facebook: String,
+        // google: String,
+    },
+    role: {
+        type: String,
+        enum: roles,
+        default: 'user',
+    },
+    picture: {
+        type: String,
+        trim: true,
+    },
+}, {
+        timestamps: true
+    });
 
 const User = mongoose.model('User', eventSchema);
 
