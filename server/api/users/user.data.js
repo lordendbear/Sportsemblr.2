a5ba7982600af5fb1e060929d54428d8d7c9a918
Ivan <madritistaaa@gmail.com>
2018-03-20 19:47:33 +0200

    Implement findByEmail

--- server/api/users/user.data.js
+++ server/api/users/user.data.js
@@ -1,5 +1,22 @@
-export default (User) => {
+export default (User, { passwordHasher }) => {
     return {
+        create: (user) => {
+            return new Promise((resolve, reject) => {
 
+            });
+        },
+        findByEmail: (email) => {
+            return new Promise((resolve, reject) => {
+                User.findOne({
+                    email
+                }, (err, user) => {
+                    if (err) {
+                        reject(err);
+                    }
+
+                    resolve(user);
+                });
+            });
+        },
     };
 }
