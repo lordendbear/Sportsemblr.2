1d2345375a8dc4a1c33709ef31fb4f09e45ea540
Ivan <madritistaaa@gmail.com>
2018-03-20 19:51:25 +0200

    Implement logic for user creation in controller

--- server/api/users/user.controller.js
+++ server/api/users/user.controller.js
@@ -1,5 +1,34 @@
 export default (data) => {
     return {
+        create: (req, res, next) => {
+            const user = req.body;
 
+            if (!user || !user.email || !user.password) {
+                return res
+                    .sendStatus(400);
+            }
+
+            data.findByEmail(user.email)
+                .then((existingUser) => {
+                    if (existingUser) {
+                        return res
+                            .status(400)
+                            .send({
+                                error: 'User already exists'
+                            });
+                    }
+
+                    return data.create(user);
+                })
+                .then(() => {
+                    return res
+                        .status(200)
+                        .send({
+                            email: user.email,
+                            name: user.name
+                        })
+                })
+                .catch(err => next(err));
+        },
     }
 }
