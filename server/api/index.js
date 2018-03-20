bb748ba4bbdbae5996f384679ca73c9077d8a0eb
Ivan <madritistaaa@gmail.com>
2018-03-20 17:58:32 +0200

    Pass app to api config

--- server/api/index.js
+++ server/api/index.js
@@ -2,11 +2,11 @@ import { Router } from 'express';
 
 import eventRouterInit from './events'
 
-export default ({ config, db }) => {
+export default (app, config, db) => {
 	const router = Router();
 
 	const eventRouter = eventRouterInit(db, config);
 	router.use('/events', eventRouter);
 
-	return router;
+	app.use('/api', router);
 }
