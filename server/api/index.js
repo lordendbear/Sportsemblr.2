9ad06e8c2ec20f2c973c5f6ac51145b147229991
Branimir123 <brani_svetoslavov@abv.bg>
2018-03-22 16:09:33 +0200

    Add place router to index.js

--- server/api/index.js
+++ server/api/index.js
@@ -2,15 +2,19 @@ import { Router } from 'express';
 
 import eventRouterInit from './events'
 import userRouterInit from './users'
+import placeRouterInit from './place';
 
 export default (app, config, utils) => {
 	const router = Router();
 
+	const userRouter = userRouterInit(config, utils);
+	router.use('/users', userRouter);
+
 	const eventRouter = eventRouterInit(config);
 	router.use('/events', eventRouter);
 
-	const userRouter = userRouterInit(config, utils);
-	router.use('/users', userRouter);
+	const placeRouter = placeRouterInit(config);
+	router.use('/places', placeRouter);
 
 	app.use('/api', router);
 }
