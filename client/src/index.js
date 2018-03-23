f2ee967ee0917cf93ad3482870761fe923e784f1
Ivan <madritistaaa@gmail.com>
2018-03-23 11:05:58 +0200

    Add path to register

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,19 @@ import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore';
-import routes from './routes';
+//import routes from './routes';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Register from './components/auth/Register'
 
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router routes={routes} />
-    </Provider>,
+        <Router >
+            <Route path="/register" component={Register} />
+        </Router >
+    </Provider >,
     document.getElementById('root')
 );
 
