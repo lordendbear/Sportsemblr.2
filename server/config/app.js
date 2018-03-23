da41f1e796818f77b56ce1a812462d3441bddcc1
Ivan <madritistaaa@gmail.com>
2018-03-23 18:11:42 +0200

    Move app initialization from index

--- server/config/app.js
+++ server/config/app.js
@@ -3,13 +3,21 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
+
 import initializePassport from './passport';
+import initializeDb from './db';
+import initializeApi from '../api';
+import initializeApp from './app';
+import initializeUtils from '../utils';
+import initializeAuthMiddleware from '../middleware/auth';
 
 export default function (config) {
     let app = express();
     app.server = http.createServer(app);
 
-    app.use(morgan('dev'));
+    if (config.env === 'dev') {
+        app.use(morgan('dev'));
+    }
 
     const { passport, strategies } = initializePassport(config);
 
@@ -20,6 +28,18 @@ export default function (config) {
 
     app.use(passport.initialize());
     passport.use('jwt', strategies.jwt);
+
+    const utils = initializeUtils(config);
+
+    const authMiddleware = initializeAuthMiddleware(passport);
+
+    const middleware = {
+        authMiddleware
+    }
+
+    initializeDb(config);
+
+    initializeApi(app, config, utils, middleware);
 
     return app;
 }
