d96f7fba98eb4ced0abf0178fc267664d366e41a
Ivan <madritistaaa@gmail.com>
2018-03-28 19:26:34 +0300

    Display list

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,15 +6,17 @@ import Navbar from './layout/Navbar';
 import Route from 'react-router-dom/Route';
 import Register from './auth/register/Register';
 import Login from './auth/login/Login';
+import EventsList from '../containers/EventsList';
 
 class App extends Component {
   render() {
     return (
       <div >
         <Navbar />
-        <div className="container">
+        <div className="container body-content">
           <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
+          <Route path="/events" component={EventsList} />
         </div>
       </div>
     );
