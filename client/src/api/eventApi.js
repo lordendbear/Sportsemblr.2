a80bf6795a58e9c6938d6f5937edba512a73d7fa
Ivan <madritistaaa@gmail.com>
2018-03-29 09:54:29 +0300

    Implement fake api for event create

--- client/src/api/eventApi.js
+++ client/src/api/eventApi.js
@@ -1,25 +1,41 @@
 const events = [
-    {
-        id: 1,
-        title: 'best match',
-        totalPeople: 12,
-        peopleNeeded: 6,
-        date: new Date(),
-        sport: {
-            name: 'Football'
-        }
-
+  {
+    id: 1,
+    title: 'best match',
+    totalPeople: 12,
+    peopleNeeded: 6,
+    date: new Date(),
+    sport: {
+      name: 'Football'
     }
+
+  }
 ]
 
+let lastId = 2;
+
 class EventApi {
-    static getAll() {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(Object.assign([], events));
-            }, 1000);
-        });
-    }
+  static getAll() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(Object.assign([], events));
+      }, 1000);
+    });
+  }
+
+  static saveEvent(event) {
+    return new Promise((resolve, reject) => {
+
+      if (event.id) {
+
+      } else {
+        event.id = lastId++;
+        events.push(event);
+      }
+
+      resolve(Object.assign({}, event));
+    });
+  }
 }
 
 export default EventApi;
