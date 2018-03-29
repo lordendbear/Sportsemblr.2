a5ac828320742a039ae5f0c099ec4722b2972ff4
Ivan <madritistaaa@gmail.com>
2018-03-29 09:37:36 +0300

    Remove console.log

--- client/src/actions/eventActions.js
+++ client/src/actions/eventActions.js
@@ -2,18 +2,17 @@ import * as types from './actionTypes';
 import EventApi from '../api/eventApi';
 
 export function loadEventsSuccess(events) {
-    return {
-        type: types.LOAD_EVENTS_SUCCESS,
-        events
-    };
+  return {
+    type: types.LOAD_EVENTS_SUCCESS,
+    events
+  };
 }
 
 export function loadEvents() {
-    return (dispatch) => {
-        return EventApi.getAll()
-            .then(events => {
-                console.log(events);
-                dispatch(loadEventsSuccess(events));
-            })
-    };
+  return (dispatch) => {
+    return EventApi.getAll()
+      .then(events => {
+        dispatch(loadEventsSuccess(events));
+      })
+  };
 }
--- server/test/unit/users/controller.test.js
+++ server/test/unit/users/controller.test.js
@@ -10,129 +10,129 @@ import initUserController from '../../../api/users/user.controller';
 import sinon from 'sinon';
 
 describe('userController', () => {
-    describe('create', () => {
-        const next = (err) => { console.log(err) };
+  describe('create', () => {
+    const next = (er) => { };
 
-        let controller = null;
-        let data = null;
+    let controller = null;
+    let data = null;
 
-        beforeEach(() => {
-            data = {
-                findByEmail: (email) => {
-                    return Promise.resolve({});
-                },
-                create: (user) => {
-                    return Promise.resolve(user);
-                }
-            }
+    beforeEach(() => {
+      data = {
+        findByEmail: (email) => {
+          return Promise.resolve({});
+        },
+        create: (user) => {
+          return Promise.resolve(user);
+        }
+      }
 
-            controller = initUserController(data);
-        });
-
-        it('Empty req.body, should sendStatus 400', () => {
-            const req = createRequestMock();
-            const res = createResponseMock();
-
-            const spy = sinon.spy(res, 'sendStatus');
-
-            controller.create(req, res);
-
-            spy.calledWith(400);
-        });
-
-        it('No user email, should sendStatus 400', () => {
-            const req = createRequestMock({ email: '' });
-            const res = createResponseMock();
-
-            const spy = sinon.spy(res, 'sendStatus');
-
-            controller.create(req, res);
-
-            spy.calledWith(400);
-        });
-
-        it('No user password, should sendStatus 400', () => {
-            const req = createRequestMock({ email: 'email' });
-            const res = createResponseMock();
-
-            const spy = sinon.spy(res, 'sendStatus');
-
-            controller.create(req, res);
-
-            spy.calledWith(400);
-        });
-
-        it('Should call data.findByEmail', () => {
-            const user = { email: 'email', password: 'password' };
-            const req = createRequestMock(user);
-            const res = createResponseMock();
-
-            const spy = sinon.spy(data, 'findByEmail');
-
-            controller.create(req, res, next);
-
-            spy.calledWith(user.email);
-        });
-
-        it('There is user, should sendStatus 400 with error message', () => {
-            const user = { email: 'email', password: 'password' };
-            const req = createRequestMock(user);
-            const res = createResponseMock();
-
-            sinon.stub(data, 'findByEmail').callsFake(() => {
-                return Promise.resolve(user);
-            });
-
-            const statusSpy = sinon.spy(res, 'sendStatus');
-            const bodySpy = sinon.spy(res, 'send');
-
-            controller.create(req, res, next);
-
-            statusSpy.calledWith(400);
-            bodySpy.calledWith({
-                error: 'User already exists'
-            });
-        });
-
-        it('There is no user, should call data.create', () => {
-            const user = { email: 'email', password: 'password' };
-            const req = createRequestMock(user);
-            const res = createResponseMock();
-
-            sinon.stub(data, 'findByEmail').callsFake(() => {
-                return Promise.resolve(false);
-            });
-
-            const spy = sinon.spy(data, 'create');
-
-            controller.create(req, res, next);
-
-            spy.calledWith(user);
-        });
-
-        it('Create successfull, should send status 200 with correct result', () => {
-            const user = { email: 'email', password: 'password', name: 'name' };
-            const req = createRequestMock(user);
-            const res = createResponseMock();
-
-            sinon.stub(data, 'findByEmail').callsFake(() => {
-                return Promise.resolve(false);
-            });
-
-            sinon.stub(data, 'create').callsFake(() => {
-                return Promise.resolve({});
-            });
-
-            const statusSpy = sinon.spy(res, 'sendStatus');
-            const bodySpy = sinon.spy(res, 'send');
-
-            controller.create(req, res, next);
-
-            statusSpy.calledWith(200);
-            bodySpy.calledWith({
-                email: user.email,
-                name: user.name
-            })
-        });
+      controller = initUserController(data);
     });
+
+    it('Empty req.body, should sendStatus 400', () => {
+      const req = createRequestMock();
+      const res = createResponseMock();
+
+      const spy = sinon.spy(res, 'sendStatus');
+
+      controller.create(req, res);
+
+      spy.calledWith(400);
+    });
+
+    it('No user email, should sendStatus 400', () => {
+      const req = createRequestMock({ email: '' });
+      const res = createResponseMock();
+
+      const spy = sinon.spy(res, 'sendStatus');
+
+      controller.create(req, res);
+
+      spy.calledWith(400);
+    });
+
+    it('No user password, should sendStatus 400', () => {
+      const req = createRequestMock({ email: 'email' });
+      const res = createResponseMock();
+
+      const spy = sinon.spy(res, 'sendStatus');
+
+      controller.create(req, res);
+
+      spy.calledWith(400);
+    });
+
+    it('Should call data.findByEmail', () => {
+      const user = { email: 'email', password: 'password' };
+      const req = createRequestMock(user);
+      const res = createResponseMock();
+
+      const spy = sinon.spy(data, 'findByEmail');
+
+      controller.create(req, res, next);
+
+      spy.calledWith(user.email);
+    });
+
+    it('There is user, should sendStatus 400 with error message', () => {
+      const user = { email: 'email', password: 'password' };
+      const req = createRequestMock(user);
+      const res = createResponseMock();
+
+      sinon.stub(data, 'findByEmail').callsFake(() => {
+        return Promise.resolve(user);
+      });
+
+      const statusSpy = sinon.spy(res, 'sendStatus');
+      const bodySpy = sinon.spy(res, 'send');
+
+      controller.create(req, res, next);
+
+      statusSpy.calledWith(400);
+      bodySpy.calledWith({
+        error: 'User already exists'
+      });
+    });
+
+    it('There is no user, should call data.create', () => {
+      const user = { email: 'email', password: 'password' };
+      const req = createRequestMock(user);
+      const res = createResponseMock();
+
+      sinon.stub(data, 'findByEmail').callsFake(() => {
+        return Promise.resolve(false);
+      });
+
+      const spy = sinon.spy(data, 'create');
+
+      controller.create(req, res, next);
+
+      spy.calledWith(user);
+    });
+
+    it('Create successfull, should send status 200 with correct result', () => {
+      const user = { email: 'email', password: 'password', name: 'name' };
+      const req = createRequestMock(user);
+      const res = createResponseMock();
+
+      sinon.stub(data, 'findByEmail').callsFake(() => {
+        return Promise.resolve(false);
+      });
+
+      sinon.stub(data, 'create').callsFake(() => {
+        return Promise.resolve({});
+      });
+
+      const statusSpy = sinon.spy(res, 'sendStatus');
+      const bodySpy = sinon.spy(res, 'send');
+
+      controller.create(req, res, next);
+
+      statusSpy.calledWith(200);
+      bodySpy.calledWith({
+        email: user.email,
+        name: user.name
+      })
+    });
+  });
 })
