dd8d3ce60abcc49e65556c5b173c9c4a46ab7574
Ivan <madritistaaa@gmail.com>
2018-03-29 10:28:04 +0300

    Dispatch notifications

--- client/src/actions/placeActions.js
+++ client/src/actions/placeActions.js
@@ -1,18 +1,23 @@
 import * as types from './actionTypes';
 import PlaceApi from '../api/placeApi';
+import * as notificationActions from './notificationActions';
 
 export function savePlaceSuccess(place) {
-    return {
-        type: types.SAVE_PLACE_SUCCESS,
-        place
-    };
+  return {
+    type: types.SAVE_PLACE_SUCCESS,
+    place
+  };
 }
 
 export function savePlace(place) {
-    return (dispatch) => {
-        return PlaceApi.savePlace(place)
-            .then(savedPlace => {
-                dispatch(savePlaceSuccess(savedPlace));
-            });
-    };
+  return (dispatch) => {
+    return PlaceApi.savePlace(place)
+      .then(savedPlace => {
+        dispatch(savePlaceSuccess(savedPlace));
+        dispatch(notificationActions.success({ message: 'Place saved.' }));
+      })
+      .catch(err => {
+        dispatch(notificationActions.error({ message: 'Something went wrong...' }));
+      });
+  };
 }
