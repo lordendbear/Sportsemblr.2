9655138201816d087d24ff6fa1155acb919d9f93
Ivan <madritistaaa@gmail.com>
2018-03-30 11:33:30 +0300

    Implement actions for update place

--- client/src/actions/placeActions.js
+++ client/src/actions/placeActions.js
@@ -9,11 +9,23 @@ export function savePlaceSuccess(place) {
   };
 }
 
+export function updatePlaceSuccess(place) {
+  return {
+    type: types.UPDATE_PLACE_SUCCESS,
+    place
+  };
+}
+
 export function savePlace(place) {
   return (dispatch) => {
     return PlaceApi.savePlace(place)
       .then(savedPlace => {
-        dispatch(savePlaceSuccess(savedPlace));
+        if (place.id) {
+          dispatch(updatePlaceSuccess(savedPlace));
+        } else {
+          dispatch(savePlaceSuccess(savedPlace));
+        }
+
         dispatch(notificationActions.success({ message: 'Place saved.' }));
       })
       .catch(err => {
