7ab965fb3dfbcc89af5b3d1b5381a6448f938e4a
Ivan <madritistaaa@gmail.com>
2018-03-30 11:08:53 +0300

    Extract reducer return case to functions

--- client/src/reducers/eventReducer.js
+++ client/src/reducers/eventReducer.js
@@ -1,11 +1,35 @@
 import * as types from '../actions/actionTypes';
 
+const handleLoadEventsSuccess = (action) => {
+    return [
+        ...action.events
+    ];
+};
+
+const handleCreateEventSuccess = (state, action) => {
+    return [
+        ...state,
+        Object.assign({}, action.event)
+    ];
+};
+
+const handleUpdateEventSuccess = (state, action) => {
+    return [
+        ...state.filter(ev => ev.id !== action.event.id),
+        Object.assign({}, action.event)
+    ];
+};
+
 export default function eventReducer(state = [], action) {
     switch (action.type) {
         case types.LOAD_EVENTS_SUCCESS:
-            return [
-                ...action.events
-            ]
+            return handleLoadEventsSuccess(action);
+
+        case types.CREATE_EVENT_SUCCESS:
+            return handleCreateEventSuccess(state, action);
+
+        case types.UPDATE_EVENT_SUCCESS:
+            return handleUpdateEventSuccess(state, action);
 
         default:
             return state;
