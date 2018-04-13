5318e3ceee27549cce4324d63a16130f08244208
Ivan <madritistaaa@gmail.com>
2018-04-13 18:22:35 +0300

    Fix typo

--- client/src/components/event/EventsList.jsx
+++ client/src/components/event/EventsList.jsx
@@ -2,44 +2,44 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import EventShort from './EventShort';
 import {
-    Badge,
-    Row,
-    Col,
-    Card,
-    CardHeader,
-    CardBody,
-    Table,
-    Pagination,
-    PaginationItem,
-    PaginationLink
-  } from 'reactstrap';
-  
+  Badge,
+  Row,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  Table,
+  Pagination,
+  PaginationItem,
+  PaginationLink
+} from 'reactstrap';
+
 const EventsList = ({ events }) => {
-    return (
-        <div className="animated fadeIn">
-        <Row>
+  return (
+    <div className="animated fadeIn">
+      <Row>
         <Col xs="12" lg="12">
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify"></i> All Events
+          <Card>
+            <CardHeader>
+              <i className="fa fa-align-justify"></i> All Events
               </CardHeader>
-              <CardBody>
-                <Table responsive striped>
-                  <thead>
+            <CardBody>
+              <Table responsive striped>
+                <thead>
                   <tr>
                     <th>Title</th>
                     <th>When</th>
-                    <th>Organizator</th>
+                    <th>Organizer</th>
                     <th>People</th>
                     <th>Status</th>
                   </tr>
-                  </thead>
-                  <tbody>
+                </thead>
+                <tbody>
                   {events.map(event =>
                     <EventShort
-                        key={event.id}
-                        event={event}
-                        isActive={true}
+                      key={event.id}
+                      event={event}
+                      isActive={true}
                     />
                   )}
                   <tr>
@@ -60,28 +60,28 @@ const EventsList = ({ events }) => {
                       <Badge color="success">Active</Badge>
                     </td>
                   </tr>
-                  </tbody>
-                </Table>
-                <Pagination>
-                  <PaginationItem disabled><PaginationLink previous href="#">Prev</PaginationLink></PaginationItem>
-                  <PaginationItem active>
-                    <PaginationLink href="#">1</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem><PaginationLink href="#">2</PaginationLink></PaginationItem>
-                  <PaginationItem><PaginationLink href="#">3</PaginationLink></PaginationItem>
-                  <PaginationItem><PaginationLink href="#">4</PaginationLink></PaginationItem>
-                  <PaginationItem><PaginationLink next href="#">Next</PaginationLink></PaginationItem>
-                </Pagination>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-        </div>
-    )
+                </tbody>
+              </Table>
+              <Pagination>
+                <PaginationItem disabled><PaginationLink previous href="#">Prev</PaginationLink></PaginationItem>
+                <PaginationItem active>
+                  <PaginationLink href="#">1</PaginationLink>
+                </PaginationItem>
+                <PaginationItem><PaginationLink href="#">2</PaginationLink></PaginationItem>
+                <PaginationItem><PaginationLink href="#">3</PaginationLink></PaginationItem>
+                <PaginationItem><PaginationLink href="#">4</PaginationLink></PaginationItem>
+                <PaginationItem><PaginationLink next href="#">Next</PaginationLink></PaginationItem>
+              </Pagination>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  )
 }
 
 EventsList.propTypes = {
-    events: PropTypes.array.isRequired,
+  events: PropTypes.array.isRequired,
 }
 
 export default EventsList;
