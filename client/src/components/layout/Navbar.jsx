686eb8ba1c4fde74dadc4551d0d22f6d2fcba285
Branimir123 <brani_svetoslavov@abv.bg>
2018-04-23 16:36:21 +0300

    Remove navbar useless links

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -41,18 +41,6 @@ class Navbar extends React.Component {
           <NavItem className="px-3">
             {this.renderNavLink('Events', '/events')}
           </NavItem>
-          <NavItem className="px-3">
-            {this.renderNavLink('Single Event', '/events/5')}
-          </NavItem>
-          <NavItem className="px-3">
-            {this.renderNavLink('Create/Edit Event', '/events/5/edit')}
-          </NavItem>
-          <NavItem className="px-3">
-            {this.renderNavLink('See/Edit Profile', '/profile')}
-          </NavItem>
-          <NavItem className="px-3">
-            {this.renderNavLink('Manage place', '/places/5')}
-          </NavItem>
         </Nav>
         <NavbarToggler className="d-md-down-none" onClick={this.asideToggle}>
           <span className="navbar-toggler-icon"></span>
