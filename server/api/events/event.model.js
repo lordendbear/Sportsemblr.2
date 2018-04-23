9d34ebec504f1eeefcbd5a6eedaf77401435e023
Branimir123 <brani_svetoslavov@abv.bg>
2018-04-23 17:00:12 +0300

    Update event model

--- server/api/events/event.model.js
+++ server/api/events/event.model.js
@@ -1,6 +1,57 @@
 import mongoose from 'mongoose';
 
 const eventSchema = new mongoose.Schema({}, {
+    title: {
+        type: String,
+        maxlength: 128,
+        index: true,
+        trim: true,
+    },
+    sport: {
+        type: String,
+        maxlength: 128,
+        index: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    place: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Place'
+    },
+    difficulty: {
+        type: String,
+        enum: ['beginner', 'advanced', 'semi-pro', 'pro']
+    },
+    picture: {
+        type: String,
+        trim: true,
+    },
+    peopleNeeded: {
+        type: Number
+    },
+    peopleJoined: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    status: {
+        type: String,
+        enum: ['active', 'started', 'ended']
+    },
+    date: {
+        type: Date
+    },
+    organizer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    totalPrice: {
+        type: mongoose.Schema.Types.Decimal128,
+
+    }
+},{
     timestamps: true
 });
 
