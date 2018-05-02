6ac47ecce5159d1fab8fa07df35f7da316fe848d
Ivan <madritistaaa@gmail.com>
2018-05-02 13:08:45 +0300

    Add logic in controller

--- server/api/users/user.controller.js
+++ server/api/users/user.controller.js
@@ -1,33 +1,45 @@
 export default (data) => {
-    return {
-        create: (req, res, next) => {
-            const user = req.body;
+  return {
+    create: (req, res, next) => {
+      const user = req.body;
 
-            if (!user || !user.email || !user.password) {
-                return res
-                    .sendStatus(400);
-            }
+      if (!user || !user.email || !user.password) {
+        return res
+          .sendStatus(400);
+      }
 
-            data.findByEmail(user.email)
-                .then((existingUser) => {
-                    if (existingUser) {
-                        return res
-                            .sendStatus(400)
-                            .send({
-                                error: 'User already exists'
-                            });
-                    }
+      data.findByEmail(user.email)
+        .then((existingUser) => {
+          if (existingUser) {
+            return res
+              .sendStatus(400)
+              .send({
+                error: 'User already exists'
+              });
+          }
 
-                    return data.create(user);
-                })
-                .then(() => {
-                    return res
-                        .send({
-                            email: user.email,
-                            name: user.name
-                        })
-                })
-                .catch(err => next(err));
-        },
+          return data.create(user);
+        })
+        .then(() => {
+          return res
+            .send({
+              email: user.email,
+              name: user.name
+            })
+        })
+        .catch(err => next(err));
+    },
+    update: (req, res, next) => {
+      const id = req.params.id,
+        options = req.body;
+
+      data.updateUser(id, options)
+        .then(user => {
+          res.send(user);
+        })
+        .catch(err => {
+          res.send(err)
+        });
     }
+  }
 }
