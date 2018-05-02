d583b6a11f068d26c63824f9f3d93aec85ea2b96
Ivan <madritistaaa@gmail.com>
2018-05-02 15:14:00 +0300

    Implement data delete

--- server/api/events/event.data.js
+++ server/api/events/event.data.js
@@ -1,5 +1,17 @@
 export default (Event) => {
-    return {
+  return {
+    deleteEvent: (id) => {
+      return new Promise((resolve, reject) => {
+        Event.remove({
+          _id: id
+        }, err => {
+          if (err) {
+            return reject(err);
+          }
 
-    };
+          return resolve();
+        });
+      })
+    }
+  };
 }
