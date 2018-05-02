e0d7312d5b941dff6cae4921e3d7c792c57a8637
Ivan <madritistaaa@gmail.com>
2018-05-02 14:30:50 +0300

    Edit user server (#58)
    * Add update logic in data
    
    * Add logic in controller
    
    * Add route for update
    
    * Implement getById
    
    * Check for existing user
    
    * Write tests
    
    * Return promise
    
    * Update tests
    
    * Change update user logic
    
    * Update test to use ObjectId

---
+++ server/test/integration/users/update.test.js
@@ -0,0 +1,76 @@
+/* eslint-env mocha */
+import { expect } from 'chai';
+import request from 'supertest';
+
+import inititalizeApp from '../../..//config/app';
+import User from '../../../api/users/user.model';
+
+import config from '../config';
+import * as testUtils from '../utils';
+
+import { Types } from 'mongoose';
+
+describe('POST /api/users/{id}', () => {
+  let dbUser;
+  let updatedUser;
+  let app;
+
+  beforeEach((done) => {
+    app = inititalizeApp(config);
+
+    dbUser = {
+      email: 'branstark@gmail.com',
+      password: 'mypassword',
+      name: 'Bran Stark',
+      role: 'admin',
+      id: 1
+    };
+
+    updatedUser = {
+      email: 'sousa.dfs@gmail.com',
+      password: '123456',
+      name: 'Daniel Sousa',
+      id: 2
+    };
+
+    User.remove({}, () => {
+      User.create(dbUser, (err, savedUser) => {
+        if (err) {
+          return done(err);
+        }
+
+        dbUser = savedUser;
+
+        return done();
+      });
+    });
+  });
+
+  afterEach((done) => {
+    testUtils.dropDatabase(done);
+  });
+
+  it('should update user data when request is ok', (done) => {
+    request(app)
+      .post(`/api/users/${dbUser.id}`)
+      .send(updatedUser)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.email).to.equal(updatedUser.email);
+        expect(res.body.name).to.equal(updatedUser.name);
+
+        done();
+      });
+  });
+
+  it('should return 404 when user does not exist', (done) => {
+    const id = new Types.ObjectId();
+    request(app)
+      .post(`/api/users/${id}`)
+      .send(updatedUser)
+      .expect(404)
+      .then(() => {
+        done();
+      });
+  });
+});
