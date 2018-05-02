4b59d8dac1c19f98bde8a0b371a01b397bc7bb8a
Ivan <madritistaaa@gmail.com>
2018-05-02 15:40:03 +0300

    Delete event server (#60)
    * Implement data delete
    
    * Add controller delete
    
    * Add route
    
    * Import promise
    
    * Add tests

--- server/api/events/event.data.js
+++ server/api/events/event.data.js
@@ -1,5 +1,19 @@
-export default (Event) => {
-    return {
+import { Promise } from 'bluebird';
 
-    };
+export default (Event) => {
+  return {
+    deleteEvent: (id) => {
+      return new Promise((resolve, reject) => {
+        Event.remove({
+          _id: id
+        }, err => {
+          if (err) {
+            return reject(err);
+          }
+
+          return resolve();
+        });
+      })
+    }
+  };
 }
