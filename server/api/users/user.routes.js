f6d7495de25cbc78130295ee2548394ac641a29d
Ivan <madritistaaa@gmail.com>
2018-05-02 14:37:08 +0300

    Setup route

--- server/api/users/user.routes.js
+++ server/api/users/user.routes.js
@@ -3,8 +3,10 @@ import { Router } from 'express';
 export default (controller) => {
   const router = new Router();
 
+
   router.post('', controller.create);
   router.post('/:id', controller.update);
+  router.get('/:id', controller.getProfile);
 
   return router;
 }
