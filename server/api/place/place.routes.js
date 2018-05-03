b38946166dad85f2edb969c074f54099cfcc7ac5
Ivan <madritistaaa@gmail.com>
2018-05-03 13:38:36 +0300

    Add route for create

--- server/api/place/place.routes.js
+++ server/api/place/place.routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 export default (controller) => {
   const router = new Router();
 
+  router.post('', controller.create);
   router.put('/:id', controller.update);
   router.delete('/:id', controller.delete);
 
