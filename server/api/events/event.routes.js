4c6832e553abb0bf4f1ae845f5f56f0d0d54d495
Ivan <madritistaaa@gmail.com>
2018-05-03 14:11:57 +0300

    Add route and controller action

--- server/api/events/event.routes.js
+++ server/api/events/event.routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 export default (controller) => {
   const router = new Router();
 
+  router.post('', controller.create);
   router.get('/:id', controller.details);
   router.delete('/:id', controller.delete);
 
