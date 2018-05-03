ee8503b79ab93d6f109becc455c3abb434b19ae7
Ivan <madritistaaa@gmail.com>
2018-05-03 11:36:47 +0300

    Implement controller action

--- server/api/events/event.controller.js
+++ server/api/events/event.controller.js
@@ -7,6 +7,19 @@ export default (data) => {
         .then(() => {
           res.sendStatus(204);
         });
+    },
+    getById: (req, res, next) => {
+      const id = req.params.id;
+
+      data
+        .getById(id)
+        .then(event => {
+          if (!event) {
+            return res.sendStatus(404);
+          }
+
+          return res.send(event);
+        });
     }
   }
 }
