e63c57030bb90097ecae98cc5f39c994efe06147
Ivan <madritistaaa@gmail.com>
2018-05-03 11:26:49 +0300

    Add route

--- server/api/place/place.routes.js
+++ server/api/place/place.routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 export default (controller) => {
   const router = new Router();
 
+  router.put('/:id', controller.update);
   router.delete('/:id', controller.delete);
 
   return router;
--- server/api/users/user.routes.js
+++ server/api/users/user.routes.js
@@ -3,7 +3,6 @@ import { Router } from 'express';
 export default (controller) => {
   const router = new Router();
 
-
   router.post('', controller.create);
   router.put('/:id', controller.update);
   router.get('/:id', controller.getProfile);
