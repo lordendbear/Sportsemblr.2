3b0c1c5e7e4852691e59a39311cc5a0db699b492
Ivan <madritistaaa@gmail.com>
2018-05-03 10:41:46 +0300

    Import places api in router

--- server/api/index.js
+++ server/api/index.js
@@ -3,18 +3,22 @@ import { Router } from 'express';
 import eventRouterInit from './events'
 import authRouterInit from './auth'
 import userRouterInit from './users'
+import placeRouterInit from './place'
 
 export default (app, config, utils, middleware) => {
-	const router = Router();
+  const router = Router();
 
-	const eventRouter = eventRouterInit(config);
-	router.use('/events', eventRouter);
+  const eventRouter = eventRouterInit(config);
+  router.use('/events', eventRouter);
 
-	const userRouter = userRouterInit(config, utils);
-	router.use('/users', userRouter);
+  const userRouter = placeRouterInit(config, utils);
+  router.use('/users', userRouter);
 
-	const authRouter = authRouterInit(config, utils);
-	router.use('/login', authRouter);
+  const placeRouter = userRouterInit(config, utils);
+  router.use('/places', placeRouter);
 
-	app.use('/api', router);
+  const authRouter = authRouterInit(config, utils);
+  router.use('/login', authRouter);
+
+  app.use('/api', router);
 }
