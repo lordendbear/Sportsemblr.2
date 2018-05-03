543e63c76056455f50fd8f0f1e449cf818227df6
Ivan <madritistaaa@gmail.com>
2018-05-03 11:36:36 +0300

    Add route

--- server/api/events/event.routes.js
+++ server/api/events/event.routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 export default (controller) => {
   const router = new Router();
 
+  router.get('/:id', controller.getById);
   router.delete('/:id', controller.delete);
 
   return router;
