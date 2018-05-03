a0ed46d7faffd321c6c90743b61d61d0a9e91f45
Ivan <madritistaaa@gmail.com>
2018-05-03 11:26:41 +0300

    Implement update logic in data and controller

--- server/api/place/place.controller.js
+++ server/api/place/place.controller.js
@@ -7,6 +7,22 @@ export default (data) => {
         .then(() => {
           res.sendStatus(204);
         });
-    }
+    },
+    update: (req, res, next) => {
+      const id = req.params.id,
+        options = req.body;
+
+      data.updatePlace(id, options)
+        .then(place => {
+          if (!place) {
+            return res.sendStatus(404);
+          }
+
+          return res.send(place);
+        })
+        .catch(err => {
+          res.send(err)
+        });
+    },
   }
 }
