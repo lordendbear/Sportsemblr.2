113dda0097f34079e12412c37490a413aed6d182
Ivan <madritistaaa@gmail.com>
2018-05-09 10:35:59 +0300

    Remove exact from navlink

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,54 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {
-    Nav,
-    NavItem,
-    NavLink,
-    NavbarToggler
-  } from 'reactstrap';
-  import HeaderDropdown from './HeaderDropdown.jsx';
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarToggler
+} from 'reactstrap';
+import HeaderDropdown from './HeaderDropdown.jsx';
 
 class Navbar extends React.Component {
-    asideToggle(e) {
-      e.preventDefault();
-      document.body.classList.toggle('aside-menu-hidden');
-    }
-      
-    renderNavLink = (label, to) => (<NavLink tag={Link} to={to} exact>
-          {label}
-      </NavLink>);
+  asideToggle(e) {
+    e.preventDefault();
+    document.body.classList.toggle('aside-menu-hidden');
+  }
 
-    render() {
-      const isAuthenticated = this.props.isAuthenticated;
+  renderNavLink = (label, to) => (<NavLink tag={Link} to={to}>
+    {label}
+  </NavLink>);
 
-      return(
+  render() {
+    const isAuthenticated = this.props.isAuthenticated;
+
+    return (
       <header className="app-header navbar">
         <Nav className="d-md-down-none" navbar>
-        { isAuthenticated && <HeaderDropdown /> }
+          {isAuthenticated && <HeaderDropdown />}
           <NavItem className="px-3">
             {this.renderNavLink('Home', '/')}
           </NavItem>
-          { !isAuthenticated && 
+          {!isAuthenticated &&
             <NavItem className="px-3">
               {this.renderNavLink('Login', '/login')}
             </NavItem>
           }
-          { !isAuthenticated && 
+          {!isAuthenticated &&
             <NavItem className="px-3">
               {this.renderNavLink('Register', '/register')}
-            </NavItem> 
+            </NavItem>
           }
           <NavItem className="px-3">
             {this.renderNavLink('Events', '/events')}
           </NavItem>
         </Nav>
-        { isAuthenticated && 
-        <NavbarToggler className="d-md-down-none" onClick={this.asideToggle}>
-          <span className="navbar-toggler-icon"></span>
-        </NavbarToggler>
+        {isAuthenticated &&
+          <NavbarToggler className="d-md-down-none" onClick={this.asideToggle}>
+            <span className="navbar-toggler-icon"></span>
+          </NavbarToggler>
         }
       </header>);
-    }
+  }
 }
 
 export default Navbar;
