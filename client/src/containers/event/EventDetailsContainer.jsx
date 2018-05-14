3d039bddfd0c90760e0ec186dcb206503e954255
Ivan <madritistaaa@gmail.com>
2018-05-14 20:19:50 +0300

    Implement details update

--- client/src/containers/event/EventDetailsContainer.jsx
+++ client/src/containers/event/EventDetailsContainer.jsx
@@ -2,23 +2,62 @@ import React from 'react';
 import { connect } from 'react-redux'
 import EventDetails from '../../components/event/EventDetails';
 import * as eventActions from '../../actions/eventActions';
+import EditEventModal from '../../components/event/EditEventModal';
 import { bindActionCreators } from 'redux';
+import { Button } from 'reactstrap';
 
 export class EventDetailsContainer extends React.Component {
     constructor(props, context) {
         super(props, context);
 
-        this.state = {}
+        this.state = {
+            isOpen: false,
+            event: Object.assign({}, props.event)
+        }
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.saveEvent = this.saveEvent.bind(this);
+        this.inputChange = this.inputChange.bind(this);
     }
 
     componentDidMount() {
         this.props.getEvent(this.props.match.params.id);
     }
 
+    componentWillReceiveProps(nextProps, ownProps) {
+        if (!ownProps.event && nextProps.event) {
+            this.setState({ event: nextProps.event });
+        }
+    }
+
     render() {
         return (
-            <EventDetails event={this.props.event}></EventDetails>
+            <div>
+                <Button color="primary" size="sm" className="float-right" onClick={this.toggleModal}>Edit</Button>
+                <EventDetails event={this.props.event}></EventDetails>
+                {this.state.isOpen && <EditEventModal closeModal={this.toggleModal} event={this.state.event} onInputChange={this.inputChange} saveEvent={this.saveEvent}></EditEventModal>}
+            </div>
         );
+    }
+
+    inputChange(value, property) {
+        let event = Object.assign({}, this.state.event);
+
+        event[property] = value;
+
+        this.setState({ event });
+    }
+
+    saveEvent() {
+        this.props.saveEvent(this.state.event)
+            .then(res => {
+                this.toggleModal();
+                this.props.getEvent(this.props.match.params.id);
+            });
+    }
+
+    toggleModal() {
+        this.setState({ isOpen: !this.state.isOpen });
     }
 }
 
@@ -30,7 +69,8 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        getEvent: bindActionCreators(eventActions.getEventById, dispatch)
+        getEvent: bindActionCreators(eventActions.getEventById, dispatch),
+        saveEvent: bindActionCreators(eventActions.saveEvent, dispatch)
     };
 };
 
