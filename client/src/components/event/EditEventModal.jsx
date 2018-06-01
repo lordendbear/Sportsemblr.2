ad7f346829c0f4a677cf399a2772d501aec87e53
Branimir123 <brani_svetoslavov@abv.bg>
2018-06-01 14:30:22 +0300

    Restrict non authenticared user from creating an event

--- client/src/components/event/EditEventModal.jsx
+++ client/src/components/event/EditEventModal.jsx
@@ -1,85 +1,86 @@
 import React from 'react';
 import Modal from 'react-bootstrap4-modal';
 import { Button, FormGroup, Input, Form, Col, Label, Row } from 'reactstrap';
+import { Redirect } from "react-router-dom";
 
-const EditEventModal = ({ event, closeModal, onInputChange, saveEvent }) => {
-  return (
-    <Modal visible={true} onClickBackdrop={closeModal} dialogClassName="modal-lg">
-      <Form onSubmit={ev => ev.preventDefault()}>
-        <FormGroup>
-          <Row>
-            <Col md="8">
-              <Label>Title</Label>
-              <Input onChange={(e) => onInputChange(e.target.value, 'title')}
-                value={event.title} type="text" />
-            </Col>
-            <Col md="4">
-              <Label>People needed</Label>
-              <Input onChange={(e) => onInputChange(e.target.value, 'peopleNeeded')}
-                value={event.peopleNeeded} type="number" />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Label>Description</Label>
-              <Input onChange={(e) => onInputChange(e.target.value, 'description')}
-                value={event.description} type="textarea" />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Label>Difficulty</Label>
-              <FormGroup check>
-                <Label check>
-                  <Input type="radio" name="radio1" />{' '}
-                  beginner
-                </Label>
-              </FormGroup>
-              <FormGroup check>
-                <Label check>
-                  <Input type="radio" name="radio1" />{' '}
-                  advanced
-                </Label>
-              </FormGroup>
-              <FormGroup check>
-                <Label check>
-                  <Input type="radio" name="radio1" />{' '}
-                  semi-pro
-                </Label>
-              </FormGroup>
-              <FormGroup check>
-                <Label check>
-                  <Input type="radio" name="radio1" />{' '}
-                  pro
-                </Label>
-              </FormGroup>
-            </Col>
-            <Col>
-              <Label>Date</Label>
-            </Col>
-          </Row>
-          <Row>
-            <Col md="6">
-              <Label>Sport</Label>
-              <Input type="select" onChange={(e) => onInputChange(e.target.value, 'sport')}
-                value={event.sport}>
-                <option>Select</option>
-                <option>Football</option>
-                <option>Basketball</option>
-                <option>Voleyball</option>
-              </Input>
-            </Col>
-          </Row>
-        </FormGroup>
+const EditEventModal = ({ isAuthenticated, event, closeModal, onInputChange, saveEvent }) => {
+  return !isAuthenticated ? 
+    <Redirect to={{ pathname: '/login' }} /> :
+    (<Modal visible={true} onClickBackdrop={closeModal} dialogClassName="modal-lg">
+        <Form onSubmit={ev => ev.preventDefault()}>
+          <FormGroup>
+            <Row>
+              <Col md="8">
+                <Label>Title</Label>
+                <Input onChange={(e) => onInputChange(e.target.value, 'title')}
+                  value={event.title} type="text" />
+              </Col>
+              <Col md="4">
+                <Label>People needed</Label>
+                <Input onChange={(e) => onInputChange(e.target.value, 'peopleNeeded')}
+                  value={event.peopleNeeded} type="number" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Label>Description</Label>
+                <Input onChange={(e) => onInputChange(e.target.value, 'description')}
+                  value={event.description} type="textarea" />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <Label>Difficulty</Label>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="radio1" />{' '}
+                    beginner
+                  </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="radio1" />{' '}
+                    advanced
+                  </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="radio1" />{' '}
+                    semi-pro
+                  </Label>
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input type="radio" name="radio1" />{' '}
+                    pro
+                  </Label>
+                </FormGroup>
+              </Col>
+              <Col>
+                <Label>Date</Label>
+              </Col>
+            </Row>
+            <Row>
+              <Col md="6">
+                <Label>Sport</Label>
+                <Input type="select" onChange={(e) => onInputChange(e.target.value, 'sport')}
+                  value={event.sport}>
+                  <option>Select</option>
+                  <option>Football</option>
+                  <option>Basketball</option>
+                  <option>Voleyball</option>
+                </Input>
+              </Col>
+            </Row>
+          </FormGroup>
 
-        <FormGroup>
-          <Button type="submit" size="xl" color="primary" onClick={saveEvent}><i className="fa fa-dot-circle-o"></i> Save</Button>
+          <FormGroup>
+            <Button type="submit" size="xl" color="primary" onClick={saveEvent}><i className="fa fa-dot-circle-o"></i> Save</Button>
 
-          <Button size="xl" color="primary" className="float-right" onClick={closeModal}><i className="fa fa-dot-circle-o"></i> Close</Button>
-        </FormGroup>
-      </Form>
-    </Modal >
-  );
+            <Button size="xl" color="primary" className="float-right" onClick={closeModal}><i className="fa fa-dot-circle-o"></i> Close</Button>
+          </FormGroup>
+        </Form>
+      </Modal >);
 }
 
 export default EditEventModal;
--- client/src/containers/EventsContainer.jsx
+++ client/src/containers/EventsContainer.jsx
@@ -9,7 +9,11 @@ class EventsContainer extends React.Component {
     constructor(props, context) {
         super(props, context);
 
-        this.state = { isOpen: false, event: Object.assign({}, props.event) };
+        this.state = { 
+            isOpen: false,
+            event: Object.assign({}, props.event),
+            isAuthenticated: this.props.isAuthenticated 
+        };
 
         this.toggleModal = this.toggleModal.bind(this);
         this.saveEvent = this.saveEvent.bind(this);
@@ -24,7 +28,7 @@ class EventsContainer extends React.Component {
         return (
             <div>
                 <EventsList events={this.props.events} onNewEventClick={this.toggleModal} ></EventsList>
-                {this.state.isOpen && <EditEventModal closeModal={this.toggleModal} event={this.state.event} onInputChange={this.inputChange} saveEvent={this.saveEvent}></EditEventModal>}
+                {this.state.isOpen && <EditEventModal isAuthenticated={this.state.isAuthenticated} closeModal={this.toggleModal} event={this.state.event} onInputChange={this.inputChange} saveEvent={this.saveEvent}></EditEventModal>}
             </div>
         );
     }
