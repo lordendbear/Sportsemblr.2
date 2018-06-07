8487c983f6f7f610e0ded5fd8cb5bd917df9245f
Ivan <madritistaaa@gmail.com>
2018-06-07 17:18:54 +0300

    Add controller method with route

--- server/api/review/review.controller.js
+++ server/api/review/review.controller.js
@@ -1,4 +1,15 @@
 export default (data) => {
   return {
+    create: (req, res, next) => {
+      const eventId = req.params.id;
+      const userId = req.user._id;
+      const review = req.body;
+
+      data.createRequest(review, userId, eventId)
+        .then((review) => {
+          console.log(review);
+          res.send(review);
+        })
+    },
   }
 }
--- server/api/review/review.routes.js
+++ server/api/review/review.routes.js
@@ -3,5 +3,7 @@ import { Router } from 'express';
 export default (controller, auth) => {
   const router = new Router();
 
+  router.post('/:id/reviews', auth, controller.create);
+
   return router;
 }
