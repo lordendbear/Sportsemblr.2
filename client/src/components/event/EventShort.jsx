d9400bb8863c83e530938697641c2fe96d0e5062
Ivan <madritistaaa@gmail.com>
2018-06-09 11:50:27 +0300

    Add link to event organizer profile in list

--- client/src/components/event/EventShort.jsx
+++ client/src/components/event/EventShort.jsx
@@ -10,7 +10,7 @@ const EventShortComponent = ({ event, isActive }) => {
         <tr>
             <td><Link to={'/events/' + event._id}>{event.title}</Link></td>
             <td>{event.date.toLocaleDateString("bg-BG")}</td>
-            <td>{event.author}</td>
+            <td><Link to={'/users/' + event.organizer._id}>{event.organizer.name}</Link></td>
             <td>{event.peopleNeeded} / {event.totalPeople}</td>
             <td>
                 <Badge color={isActive ? 'success' : 'danger'}>{isActive ? 'Active' : 'Inactive'}</Badge>
