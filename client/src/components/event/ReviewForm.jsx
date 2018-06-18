6103bfd9d5df697d7b221bdd523cbf2c316830cc
Ivan <madritistaaa@gmail.com>
2018-06-18 17:33:42 +0300

    Remove unused import

--- client/src/components/event/ReviewForm.jsx
+++ client/src/components/event/ReviewForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, FormGroup, Input, Form, Col, Label, Row, Card, CardBody, CardTitle, CardText, CardSubtitle } from 'reactstrap';
+import { Button, FormGroup, Input, Form, Col, Label, Row, Card, CardBody, CardTitle, CardText } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 export class ReviewForm extends React.Component {
