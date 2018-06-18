a3e12ee4b0274164ce7a1982c31bf6a32e6edbe3
Ivan <madritistaaa@gmail.com>
2018-06-18 20:14:53 +0300

    Add backend logic for delete message

--- server/api/messages/message.routes.js
+++ server/api/messages/message.routes.js
@@ -4,6 +4,7 @@ export default (controller, auth) => {
   const router = new Router();
 
   router.get('/:id/messages', auth, controller.getMessages);
+  router.delete('/:id/messages/:messageId', auth, controller.deleteMessage);
 
   return router;
 }
